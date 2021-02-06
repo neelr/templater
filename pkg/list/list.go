64fcbb9f19e53eced7b0085635a82751bb8bb2c8
Neel Redkar <neel.redkar@gmail.com>
2021-02-06 01:34:41 -0800

    add correct template sizes

--- pkg/list/list.go
+++ pkg/list/list.go
@@ -3,11 +3,27 @@ package list
 import (
 	"io/ioutil"
 	"os"
-	"strconv"
+	"fmt"
+	"path"
+	"path/filepath"
 
 	log "github.com/neelr/templater/pkg/logs"
 	"github.com/neelr/templater/pkg/setup"
 )
+
+func DirSize(path string) (int64, error) {
+    var size int64
+    err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
+        if err != nil {
+            return err
+        }
+        if !info.IsDir() {
+            size += info.Size()
+        }
+        return err
+    })
+    return size, err
+}
 
 func Command() error {
 	setup.Configs()
@@ -19,8 +35,9 @@ func Command() error {
 	log.InformationPrint("All Templates")
 	log.InformationPrint("------------------")
 	for _, f := range templates {
+		size, _ := DirSize(path.Join(os.Getenv("PLATE_DIR"),f.Name())); 
 		if f.IsDir() {
-			log.NormalPrint(f.Name() + " - " + strconv.FormatInt(f.Size(), 10) + " bytes")
+			log.NormalPrint(f.Name() + " - " + fmt.Sprintf("%.2f",float32(size)/1000.0) + " KB")
 		}
 	}
 	return nil
